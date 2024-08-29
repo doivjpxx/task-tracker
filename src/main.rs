367eb50d0ad8755b46e339f132ced66e336d4241
phong.bnh <doivjpxx@gmail.com>
2024-08-29 20:15:11 +0700

    feat: update main fn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use task::{Task, TaskImpl};
+use task_list::{TaskList, TaskListImpl};
+
+mod task;
+mod task_list;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Task tracker CLI application");
+    let task_list = TaskList::new();
+
+    let task = Task::new(1, "Learn Rust".to_string());
+
+    println!("{:?}", task);
+    println!("{:?}", task_list);
 }
